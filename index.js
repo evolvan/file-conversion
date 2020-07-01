dcee0e09430b76ad9d2ed4db06a05f7981da2025
MADS_STUDIO <sagark@evolvan.com>
2020-07-01 23:17:51 +0530

    Dropdown is working now

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express'),
-    user = require('./routers/User.js'),
-    app = express();
-app.use(helmet());
-app.use(cors());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json());
-app.set('trust proxy', true);
+// const express = require('express')
+    
+// app = express();
 
-//map router files to respective urls
-//these are stored in a directory and set above, these contain all of the handlers for each of my routes
-app.use('/user', user);
+let csvToJson = require('convert-csv-to-json');
+ 
+    let fileInputName = './SalesJan2009.csv'; 
+    let fileOutputName = './myOutputFile.json';
+     
+    csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
 
-//set port and listen on it
-app.listen(5000, () => console.log("Server running on port 5000"));
+    // app.listen(5000, () => console.log("Server running on port 5000"));
+
--- public/js/main.js
+++ public/js/main.js
@@ -8,114 +8,82 @@ function getSelectedValue(){
         console.log("Please Choose The Option From DropDown !");
     }
 }
-
 window.onload = function(){ 
-        document.getElementById('btnSubmit').onclick = function() {
-            document.getElementById('frmUploader').submit();
-            fileName = document.querySelector('#e').innerHTML; 
-            console.log(fileName,"sagar");
-            extension = fileName.split('.').pop(); 
-            console.log(extension,"ddddddddddddddddddddd"); 
-            // Start PDF Dropdown validation
-            if(extension=='pdf'){
-            var values = [".doc", ".docx", ".pdf"];
-          
-            var select = document.createElement("select");
-            select.name = "fileExt";
-            select.id = "fileFormat";
-            select.classList.add("fileFormat");
-            select.setAttribute("onchange", function(){getSelectedValue()});
-          
-            for (const val of values) {
-              var option = document.createElement("option");
-              option.value = val;
-              option.text = val.charAt(0).toUpperCase() + val.slice(1);
-              select.appendChild(option);
-            }
-          
-            var label = document.createElement("label");
-            label.innerHTML = "Choose your File Extension: "
-            label.htmlFor = "Files";
-          
-            document.getElementById("downloadFile").appendChild(label).appendChild(select);
-          }
-          // End PDF Dropdown validation
-            // Start Image Dropdown validation
-            else if(extension=='png'){
-                document.getElementById('frmUploader').submit();
-            var values = [".jpg", ".bmp", ".jpeg",".tiff",".gif",".png",".pdf"];
-          
-            var select = document.createElement("select");
-            select.name = "fileExt";
-            select.id = "fileFormat";
-            select.classList.add("fileFormat");
-            select.setAttribute("onchange", function(){getSelectedValue()});
-          
-            for (const val of values) {
-              var option = document.createElement("option");
-              option.value = val;
-              option.text = val.charAt(0).toUpperCase() + val.slice(1);
-              select.appendChild(option);
-            }
-          
-            var label = document.createElement("label");
-            label.innerHTML = "Choose your File Extension: "
-            label.htmlFor = "Files";
-          
-            document.getElementById("downloadFile").appendChild(label).appendChild(select);
-          }
-          // End Image Dropdown validation
-            // Start word Dropdown validation
-            else if(extension=='doc'){
-                document.getElementById('frmUploader').submit();
-                var values = [".docx", ".pdf", ".doc"];
-              
-                var select = document.createElement("select");
-                select.name = "fileExt";
-                select.id = "fileFormat";
-                select.classList.add("fileFormat");
-                select.setAttribute("onchange", function(){getSelectedValue()});
-              
-                for (const val of values) {
-                  var option = document.createElement("option");
-                  option.value = val;
-                  option.text = val.charAt(0).toUpperCase() + val.slice(1);
-                  select.appendChild(option);
+    // fileClass = document.getElementsByClassName(".inner-content");
+
+    fileName = document.querySelector('.inner-content p').innerHTML; 
+    for(let i of fileName){
+
+        console.log(fileName,"sagar");
+        extension = fileName.split('.').pop(); 
+        console.log(extension,"ddddddddddddddddddddd"); 
+    
+    }    
+    let displayOption = document.getElementsByTagName("option");
+        
+    document.getElementById('fileFormat').onclick = function() {
+            
+
+            for (var i = 0; i<displayOption.length; i++) {
+                // let optionValue = document.getElementsByTagName("option")[i].value;
+                console.log("hihihih",displayOption[i].value);
+                // pdf converted extension
+                if (extension=='pdf') {
+                    var values = [".doc", ".docx", ".pdf"];
+                    for (const val of values) {
+                        console.log(val);
+                        if(val == displayOption[i].value){
+                            console.log("val",val);
+                            console.log("displayOption[i].value",displayOption[i].value);
+                            displayOption[i].style.display = 'block';
+                        }
+                      }   
                 }
-              
-                var label = document.createElement("label");
-                label.innerHTML = "Choose your File Extension: "
-                label.htmlFor = "Files";
-              
-                document.getElementById("downloadFile").appendChild(label).appendChild(select);
-              }
-              // End word Dropdown validation
-            // Start excel Dropdown validation
-            else if(extension=='xls'){
-                document.getElementById('frmUploader').submit();
-                var values = [".xlsm", ".xlsx", ".xls",".ods"];
-              
-                var select = document.createElement("select");
-                select.name = "fileExt";
-                select.id = "fileFormat";
-                select.classList.add("fileFormat");
-                select.setAttribute("onchange", function(){getSelectedValue()});
-              
-                for (const val of values) {
-                  var option = document.createElement("option");
-                  option.value = val;
-                  option.text = val.charAt(0).toUpperCase() + val.slice(1);
-                  select.appendChild(option);
+                // pdf converted extension end
+                // Word File converted extension
+                else if(extension=='doc'){
+                    var values = [".docx", ".pdf", ".doc"];
+                        for (const val of values) {
+                            console.log(val);
+                            if(val == displayOption[i].value){
+                                console.log("val",val);
+                                console.log("displayOption[i].value",displayOption[i].value);
+                                displayOption[i].style.display = 'block';
+                            }
+                       }   
+                    }
+                // Word File converted extension end
+                // Image File converted extension
+                else if(extension=='png'){
+                    var values = [".jpg", ".bmp", ".jpeg",".tiff",".gif",".png",".pdf"];
+                        for (const val of values) {
+                            console.log(val);
+                            if(val == displayOption[i].value){
+                                console.log("val",val);
+                                console.log("displayOption[i].value",displayOption[i].value);
+                                displayOption[i].style.display = 'block';
+                            }
+                       }   
+                    }
+                // Image File converted extension end
+                // Excel File converted extension
+                else if(extension=='xls'){
+                    var values = [".xlsm", ".xlsx", ".xls",".ods"];
+                        for (const val of values) {
+                            console.log(val);
+                            if(val == displayOption[i].value){
+                                console.log("val",val);
+                                console.log("displayOption[i].value",displayOption[i].value);
+                                displayOption[i].style.display = 'block';
+                            }
+                       }   
+                    }
+                // Excel File converted extension end
+                else{
+                    alert("Please Wait Applicable Format is Setting.");
                 }
-              
-                var label = document.createElement("label");
-                label.innerHTML = "Choose your File Extension: "
-                label.htmlFor = "Files";
-              
-                document.getElementById("downloadFile").appendChild(label).appendChild(select);
-              }
-              // End excel Dropdown validation
             }
+        }
 }
 
 
